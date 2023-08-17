1ba977e908aa08eb9f6f9b3c6161c4dd6e73bad4
abdul Gafaru <kenneth.danso@mybestquote.com>
2023-08-17 12:47:21 +0000

    setting up routing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,11 @@ import { BsFillMusicPlayerFill, BsHeadphones } from 'react-icons/bs';
 import { AiFillHome, AiFillHeart, AiOutlineCloseSquare } from 'react-icons/ai';
 import { RiCompassDiscoverLine } from 'react-icons/ri';
 import { BiMenu } from 'react-icons/bi';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Search from "../pages/discover/Search";
+import Playlist from "../pages/discover/Playlist";
+import Favourite from "../pages/discover/Favourite";
+import Login from "../pages/discover/Login";
 
 function Sidebar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -20,33 +25,51 @@ function Sidebar() {
       </div>
 
       {/* Sidebar */}
+      <Router>
       <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
         <div className="logo">
           <BsFillMusicPlayerFill className="icon" />
-          <h1>HEROPLAY</h1>
+          <h1>HEROPLAY</h1> 
         </div>
 
         <div className="menu-items">
           <div className="item active">
+            <Link to="/">
             <AiFillHome />
             <p>Home</p>
+            </Link>
           </div>
           <div className="item">
+            <Link to="/search">
             <RiCompassDiscoverLine />
             <p>Discover</p>
+            </Link>
           </div>
           <div className="item">
+            <Link to="/playlist">
             <BsHeadphones />
             <p>Playlists</p>
+            </Link>
           </div>
           <div className="item">
+            <Link to="/favourite">
             <AiFillHeart />
             <p>Favourites</p>
+            </Link>
           </div>
         </div>
         <hr />
         <button className="btn">Log In</button>
       </div>
+
+      <Routes>
+        <Route path="/search" element={<Search />} />
+        <Route path="/favourite" element={<Favourite />} />
+        <Route path="/playlist" element={<Playlist />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<h4>Page Not Found!</h4>} />
+      </Routes>
+      </Router>
     </>
   );
 }
